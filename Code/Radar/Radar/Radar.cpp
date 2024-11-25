f2d03b77402dc9186295bce4cfbefecdd7109bf2
lhckmn <leander.hackmann@gmail.com>
2024-11-25 16:34:15 +0100

    Added multithreading. Access security to be implemented

--- Code/Radar/Radar/Radar.cpp
+++ Code/Radar/Radar/Radar.cpp
@@ -2,27 +2,72 @@
 //
 
 #include "Radar.h"
-#include "IWR6843.h"
-#include "SensorData.h"
 
-using namespace std;
+IWR6843 sensor;
 
+const int NUM_THREADS = 3;
+pthread_t threads[NUM_THREADS];
 
 int main() {
-    IWR6843 sensor = IWR6843();
+    
+    //Initializing the sensor
+    sensor = IWR6843();
     sensor.init("/dev/ttyUSB0", "/dev/ttyUSB1", "../configs/xwr68xx_AOP_profile_2024_10_31T16_15_25_003.cfg");
+
+    //Creating an array holding the function pointers for the threads
+    void* (*thread_functions[NUM_THREADS])(void*) =
+    {
+        sensor_thread,
+        controller_thread,
+        actuator_thread
+    };
+    
+    //Creating the threads
+    for (int i = 0; i < NUM_THREADS; i++)
+    {
+        if (pthread_create(&threads[i], nullptr, thread_functions[i], nullptr) != 0)
+        {
+            cout << "Error creating thread with ID " << i + 1 << endl;
+            return -1;
+        }
+    }
+
+    //Joining the threads
+    for (int i = 0; i < NUM_THREADS; i++)
+    {
+        if (pthread_join(threads[i], nullptr) != 0)
+        {
+            cout << "Error joining thread with ID " << i + 1 << endl;
+            return -1;
+        }
+    }
+
+    //Returning a 1 after joining all threads (may not be reached but for the sake of completeness)
+    cout << "Successfully joined all threads" << endl;
+    return 1;
+}
+
+/// <summary>
+/// Function of the sensor thread
+/// </summary>
+/// <param name="arg"></param>
+/// <returns></returns>
+void* sensor_thread(void* arg)
+{
+    //Obtaining the thread's ID
+    int thread_id = pthread_self();
 
     while (true)
     {
         //Polling the sensor and getting the amount of recently received frames
         int numOfNewFrames = sensor.poll();
-        
+
         //Processing if any new frames were received
         if (numOfNewFrames > 0)
         {
             //Getting and deleting the new frames from the buffer of decoded frames
             vector<SensorData> newFrames = sensor.getDecodedFramesFromTop(numOfNewFrames, true);
-            
+
             //Iterating over all new frames and printing out the x,y,z,doppler values
             for (int i = 0; i < newFrames.size(); i++)
             {
@@ -39,5 +84,49 @@ int main() {
             }
         }
     }
-    return 0;
+
+    //Exiting the thread
+    pthread_exit(nullptr);
+}
+
+/// <summary>
+/// Function of the controller thread
+/// </summary>
+/// <param name="arg"></param>
+/// <returns></returns>
+void* controller_thread(void* arg)
+{
+    //Obtaining the thread's ID
+    int thread_id = pthread_self();
+
+    //Simulating work
+    cout << "Hello from thread " << thread_id << endl;
+    while (true)
+    {
+        sleep(10);
+    }
+    
+    //Exiting the thread
+    pthread_exit(nullptr);
+}
+
+/// <summary>
+/// Function of the acutator thread
+/// </summary>
+/// <param name="arg"></param>
+/// <returns></returns>
+void* actuator_thread(void* arg)
+{
+    //Obtaining the thread's ID
+    int thread_id = pthread_self();
+    
+    //Simulating work
+    cout << "Hello from thread " << thread_id << endl;
+    while (true)
+    {
+        sleep(10);
+    }
+    
+    //Exiting the thread
+    pthread_exit(nullptr);
 }
--- Code/Radar/Radar/Radar.h
+++ Code/Radar/Radar/Radar.h
@@ -1,16 +1,29 @@
 ﻿#ifndef RADAR_H
 #define RADAR_H
 
+//Global includes
 #include <cstdint>
 #include <vector>
 #include <string>
 #include <iostream>
+#include <pthread.h>
 
-// Define reading modes
-#define CONTINUOUS_READING 1       // 1 for continuous, 0 for limited frames
-#define FIXED_FRAME_COUNT 10       // Set the number of frames to read if continuous is disabled
+//Private includes
+#include "IWR6843.h"
+#include "SensorData.h"
 
+//Namespaces
+using namespace std;
 
-extern std::vector<uint8_t> values;
+//External variables
+extern IWR6843 sensor;
+
+extern const int NUM_THREADS;
+extern pthread_t threads[];
+
+//Function prototypes
+void* sensor_thread(void* arg);
+void* controller_thread(void* arg);
+void* actuator_thread(void* arg);
 
 #endif // RADAR_H
